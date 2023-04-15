61fe31a28da8977007298ca5e0d91b992accacce
Инал Гагиев <igagiev198@gmail.com>
2023-04-15 20:48:32 +0300

    add category

--- crud/forms.py
+++ crud/forms.py
@@ -1,12 +1,17 @@
 from django import forms
-from .models import CrudModel
+from .models import *
 from tinymce.widgets import TinyMCE
 
 
 class ImageForm(forms.ModelForm):
     class Meta:
         model = CrudModel
-        fields = ('title', 'full_text')
+        fields = ('title', 'full_text', 'category')
         widgets = {
             "full_text": TinyMCE(mce_attrs={'cols': 80, 'rows': 30})
         }
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ['name']
--- crud/models.py
+++ crud/models.py
@@ -3,12 +3,20 @@ from django.contrib.auth.models import User
 from tinymce import models as tinymce_models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
 class CrudModel(models.Model):
     title = models.CharField(max_length=50)
     full_text = tinymce_models.HTMLField('полный текст', null=True)
     date = models.DateTimeField(null=True)
     # upload_to равносилен media/images
     image = models.ImageField(upload_to='images', null=True)
+    # тут мы используем тип связи ForeignKey
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return f'{self.title}'
